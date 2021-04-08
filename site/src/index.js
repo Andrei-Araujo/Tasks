48e72b491408f2a99a1fa083caa42960cbd99faf
Andrei-Araujo <66495784+Andrei-Araujo@users.noreply.github.com>
2021-04-08 12:33:51 -0300

    Redux Persist
    - Implementação da base para uso de Redux Persist

--- site/src/index.js
+++ site/src/index.js
@@ -7,6 +7,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { applyMiddleware, createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import { App } from "./App";
 import "./index.css";
@@ -15,11 +18,23 @@ import reducers from "./reducers";
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const storeWithMiddleware = createStoreWithMiddleware(reducers);
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={storeWithMiddleware}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
